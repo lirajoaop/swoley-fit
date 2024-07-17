c309048d299eaa0865449151365a0803d1386050
lirajoaop <joaopedroababa132@gmail.com>
2024-07-17 14:48:55 -0300

    header within generator component

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,12 +1,30 @@
 import React from "react";
 import SectionWrapper from "./SectionWrapper";
 
+function Header(props) {
+  const { index, title, description } = props;
+  return (
+    <div>
+      <div>
+        <p>{index}</p>
+        <h4>{title}</h4>
+      </div>
+      <p>{description}</p>
+    </div>
+  );
+}
+
 const Generator = () => {
   return (
     <SectionWrapper
       header={"generate your workout"}
       title={["It's", "Huge", "o'clock"]}
     >
+      <Header
+        index={"01"}
+        title={"Pick your poison"}
+        description={"Select the workout you wish to enjoy"}
+      />
       dasdasdsas
     </SectionWrapper>
   );
