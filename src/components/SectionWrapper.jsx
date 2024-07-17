dcc70e8dbbc394c1b09929a9db9bee2cb4313155
lirajoaop <joaopedroababa132@gmail.com>
2024-07-17 15:25:50 -0300

    generator buttons

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -12,7 +12,9 @@ const SectionWrapper = (props) => {
           {title[2]}
         </h2>
       </div>
-      {children}
+      <div className="max-w-[800px] w-full flex flex-col mx-auto gap-10">
+        {children}
+      </div>
     </section>
   );
 };
