fb4b71d7ef20ebd7c538b48dca8905616ec8ef78
lirajoaop <joaopedroababa132@gmail.com>
2024-07-19 21:18:44 -0300

    adjusting grid cols on different screens

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -147,7 +147,7 @@ const Generator = (props) => {
         description={"Select your ultimate objective."}
       />
 
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
         {Object.keys(SCHEMES).map((scheme, SchemeIndex) => {
           return (
             <button
