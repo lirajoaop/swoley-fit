d50623965bd265a60d216c2d8ec42593541fc655
lirajoaop <joaopedroababa132@gmail.com>
2024-07-19 22:55:11 -0300

    creating ExerciseCard component and adding "Welcome to the DANGER zone"

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,8 +1,18 @@
 import React from "react";
+import SectionWrapper from "./SectionWrapper";
+import ExerciseCard from "./ExerciseCard";
 
 const Workout = (props) => {
   const { workout } = props;
-  return <div>Workout</div>;
+  return (
+    <SectionWrapper header={"welcome to"} title={["The", "DANGER", "zone"]}>
+      <div className="flex flex-col gap-4">
+        {workout.map((exercise, i) => {
+          return <ExerciseCard exercise={exercise} key={i} />;
+        })}
+      </div>
+    </SectionWrapper>
+  );
 };
 
 export default Workout;
